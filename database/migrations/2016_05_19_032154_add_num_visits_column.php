dc885f5c40f72de80762eb2ae2d24b583721e948
HomerGaidarski <homer.gaidarski@gmail.com>
2016-05-19 04:46:49 +0000

    I have been overwriting migrations instead of making new ones which caused some issues when I forgot to rollback first; from now on, when making changes to database just do new migration rather than editing existing one; so now there is a migration for creating urls table and then a different migration for adding the num_visits column

--- database/migrations/2016_05_19_032154_add_num_visits_column.php
+++ database/migrations/2016_05_19_032154_add_num_visits_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUrlsTable extends Migration
+class AddNumVisitsColumn extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,9 @@ class CreateUrlsTable extends Migration
      */
     public function up()
     {
-        Schema::create('urls', function(Blueprint $table) {
-	    $table->increments('id');
-	    $table->timestamps();
-	    $table->string('long_url');
-        $table->bigInteger('num_visits');
-	});
+        Schema::table('urls', function ($table) {
+	        $table->bigInteger('num_visits');
+	    });
     }
 
     /**
@@ -27,6 +24,8 @@ class CreateUrlsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('urls');
+        Schema::table('urls', function ($table) {
+            $table->dropColumn('num_visits');
+        });
     }
 }
